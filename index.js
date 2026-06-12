6314d61bd46c47ce8b6adfc498255ba62fd99a87
huanchenshang <840943839@qq.com>
2026-06-12 12:56:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-require('./app.js')
+const app = require('./app.js');
+
+module.exports = async (req, res) => {
+  // 确保请求能够正确分发给后端的 Express 实例
+  if (typeof app === 'function') {
+    return app(req, res);
+  } else if (app && typeof app.serveNcmApi === 'function') {
+    return app.serveNcmApi(req, res);
+  }
+  res.status(500).send('Server initialization failed.');
+};
